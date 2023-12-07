6cbb41b2e1fa0e9ab77acf6fe35736d55a825a54
Jonathan Young <jyoun127@jhu.edu>
2023-12-07 14:24:22 -0500

    end to end pipeline created

--- run.sh
+++ run.sh
@@ -1,14 +1,35 @@
+
+
+################## UNCOMMENT BELOW FOR TESTING ###############################
+
 # Cleans the project
-rm -f *.coverage
-dune clean
+dune clean 
 
-# Runs Coverage
-find . -name '*.coverage' | xargs rm -f
-dune runtest --instrument-with bisect_ppx --force
-
-# Displays the report
-bisect-ppx-report html
-firefox ./_coverage/index.html
-
-# builds the project
+# Builds
+cd src
 dune build
+
+# test files
+files="./testPythonFiles/sample1.py"
+
+# Runs the tests
+location=$(/home/john/Documents/proj/_build/default/src/convert.exe $files)
+echo $location
+
+cd ..
+
+################## UNCOMMENT BELOW FOR COVERAGE AND PACKAGING ##################
+# Cleans the project
+# rm -f *.coverage
+# dune clean
+
+# # Runs Coverage
+# find . -name '*.coverage' | xargs rm -f
+# dune runtest --instrument-with bisect_ppx --force
+
+# # Displays the report
+# bisect-ppx-report html
+# firefox ./_coverage/index.html
+
+# # builds the project
+# dune build
--- src/convert.ml
+++ src/convert.ml
@@ -1,3 +1,54 @@
 (* Compiles to executable, entrypoint for CLI *)
 
-let () = Stdio.printf "hello world\n"
+
+
+open Core
+
+
+module FileIO = struct
+(*reads in the file*)
+  let readFile input =
+    let file = In_channel.create input in
+    let lines = In_channel.input_lines file in
+    In_channel.close file;
+    List.fold ~init:"" ~f:(fun acc x -> acc ^ x ^ " ") lines 
+
+  (*writes output file*)
+  let writeFile ~output:output ~input:input =
+    let file = Out_channel.create output in
+    Out_channel.output_string file input;
+    Out_channel.close file
+end
+
+let renamePyFileToC fileName = 
+  let fileName = String.split_on_chars fileName ~on:['.'] in
+  let fileName = List.hd_exn fileName in
+  fileName ^ ".c"
+
+
+(*reads in the file*)
+
+let run_ops (listOfFiles:string list) =
+  let rec helper listOfFiles = 
+    match listOfFiles with
+    | [] -> ()
+    | currentFile::t -> 
+      let outputFileC = renamePyFileToC currentFile in
+      let content = FileIO.readFile currentFile |> Parse.toast |> Codegen.ConModule.convertToString in 
+      FileIO.writeFile ~output:outputFileC ~input:content;
+      helper t
+  in helper listOfFiles
+
+(* command lines *)
+let command =
+  Command.basic
+    ~summary:
+      "convert.exe --files <file1> <file2>"
+    ~readme:(fun () -> "More detailed information")
+    Command.Let_syntax.(
+      let%map_open
+        files = anon (sequence ("files" %: string))
+      in
+      fun () -> run_ops files)
+
+let () = Command_unix.run command
