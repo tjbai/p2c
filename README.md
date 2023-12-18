a153676da6dcf2ab4d6c2ae85c519b8b43b05a50
Jonathan Young <jyoun127@jhu.edu>
2023-12-18 13:29:55 -0500

    README updates

--- README.md
+++ README.md
@@ -1,7 +1,43 @@
 ## P2C 
 
 #### Project Description 
-Conversion of Python files to C/C++
+Conversion of Python files to C
+
+#### Final Product 
+##### How to Run
+
+Uncomment `run.sh` to test functionality and look at code coverage (be sure to uncomment portions that are required to run)
+
+Uncomment `/src/testPythonFiles` to test functionalty for compiling python files into c, and running those files in C. 
+
+#### File Structure
+
+##### `src`
+
+`ast.ml/mli` AST Tree Declaration
+
+`parse.ml/mli` Parses the python file
+
+`lex.ml/mli` Converts the parsed elements into an AST tree
+
+`codegen.ml/mli` Converts AST into C equivalent (including header and source files)
+
+`codegenutil.ml/mli` Helper functions for converting a python file to c
+
+`convert.ml/mli` Entry point for our CLI.
+
+##### `test`
+
+`codegenTests.ml` - tests for code generation
+
+`endtoendtests.ml` - tests for end to end pipeline testing
+
+`lextests.ml` - testing for lexing
+
+`parsetests.ml` - testing for parsing 
+
+`tests.ml` - unused - place holder for additional tests
+
 
 #### Checkpoint 
 **What's been Implemented** 
@@ -19,4 +55,3 @@ End to end testing
 Writing functions for file I/O
 
 Header files
-
---
+++ tests/endtoendtests.ml
@@ -0,0 +1,163 @@
+open OUnit2
+
+let e2e source = source |> Parse.to_ast |> Codegen.ConModule.convertToString
+
+(************** UTILITIES **************)
+
+  let string_to_list str =
+    let rec loop i limit =
+      if i = limit then []
+      else str.[i] :: loop (i + 1) limit
+    in
+    loop 0 (String.length str)
+  let escape_chars (s : string) : string =
+  let escape_char = function
+    | '\n' -> "\\n"
+    | '\t' -> "\\t"
+    | '\\' -> "\\\\"
+    | c -> String.make 1 c
+  in
+  String.concat "" (List.map escape_char (  s |> string_to_list ))   
+
+(************** FUNCTION GENERATION TESTS **************)
+
+let withoutMain_eg_py =
+  "def sampleFunction(a: int, b: int) -> int:\n\
+   \treturn a + b\n\n\
+   e = 5\n\
+   f = 6\n\
+   e + f"
+
+let withoutMain_eg_c =
+  "int sampleFunction(int a, int b){\n\treturn a + b;\n}\nint main(){\n\tint e = 5;\n\tint f = 6;\n\te + f;\n}\n"  
+  
+let testFunctions _ =
+  (* Printf.printf "%s\n" @@ escape_chars @@ (withoutMain_eg_py |> e2e); *)
+  
+  assert_equal withoutMain_eg_c @@ (withoutMain_eg_py |> e2e)
+
+(**************** CONDITIONAL TESTS **************************)
+
+let if_eg_py =
+  "def sampleFunction(a: int, b: int) -> int:\n\
+   \tif a > b:\n\
+   \t\treturn a\n\
+   \telse:\n\
+   \t\treturn b"
+
+let if_eg_c =
+"int sampleFunction(int a, int b){\n\tif(a > b){\n\t\treturn a;\n\t}else{\n\t\treturn b;\n\t}\n}\n"
+
+let if_elif_eg_py =
+  "def sampleFunction(a: int, b: int) -> int:\n\
+   \tif a > b:\n\
+   \t\treturn a\n\
+   \telif a < b:\n\
+   \t\treturn b\n\
+   \telse:\n\
+   \t\treturn a + b"
+
+let if_elif_eg_c ="int sampleFunction(int a, int b){\n\tif(a > b){\n\t\treturn a;\n\t}else if(a < b){\n\t\treturn b;\n\t}else{\n\t\treturn a + b;\n\t}\n}\n"
+let testIf _ =
+  (* Printf.printf "%s\n" @@ escape_chars @@ (if_elif_eg_py |> e2e); *)
+  assert_equal if_eg_c @@ (if_eg_py |> e2e);
+
+  assert_equal if_elif_eg_c @@ (if_elif_eg_py |> e2e)
+
+(****************************** FUNCTION CALLS TESTS ******************************)
+let funcCall_py =
+  "def sampleFunction(a: int, b: int) -> int:\n\
+   \twhile a > b:\n\
+   \t\ta -= 1\n\
+   \treturn a\n\n\
+   def callFunction():\n\
+   \treturn sampleFunction(10, 5)\n\n\
+   callFunction()"
+
+let funcCall_c =
+"int sampleFunction(int a, int b){\n\twhile(a > b){\n\t\ta -= 1;\n\t}\n\treturn a;\n}\nvoid callFunction(){\n\treturn sampleFunction(10, 5);\n}\nint main(){\n\tcallFunction();\n}\n"
+let testFuncCall _ = 
+  (* Printf.printf "%s\n" @@ escape_chars @@ (funcCall_py |> e2e); *)
+  assert_equal funcCall_c @@ (funcCall_py |> e2e)
+
+(********************** FUNCTIONS WITH COMMENTS TESTS **************************)
+
+let funcWithComments_py =
+  "def sampleFunction(a: int, b: int) -> int:\n\
+   \t# HELLO! THIS IS A COMMENT!\n\
+   \tif a > b:\n\
+   \t\treturn a\n\
+   \telse:\n\
+   \t\treturn b"
+
+let funcWithComments_c = "int sampleFunction(int a, int b){\n\t//HELLO! THIS IS A COMMENT!\n\tif(a > b){\n\t\treturn a;\n\t}else{\n\t\treturn b;\n\t}\n}\n"
+
+let testFuncWithComments _ =
+  (* Printf.printf "%s\n" @@ escape_chars @@ (funcWithComments_py |> e2e); *)
+  assert_equal funcWithComments_c @@ (funcWithComments_py |> e2e)
+
+(************************** TYPE INFERENCING TESTS **************************)
+let inferencing_1 =
+  "def sampleFunction(a: int, b: int) -> int:\n\
+   \t# HELLO! THIS IS A COMMENT!\n\
+   \tc = a + b\n\
+   \treturn c"
+
+let inferencing_1_c =
+"int sampleFunction(int a, int b){\n\t//HELLO! THIS IS A COMMENT!\n\tint c = a + b;\n\treturn c;\n}\n"
+
+let inferencing_2 =
+  "def foo(a: int, b: int) -> int:\n\
+   \treturn a+b\n\n\
+   def test(c: int) -> int:\n\
+   \treturn c\n\n\
+   i = foo(2,3) + test(4)"
+
+let inferencing_2_c ="int foo(int a, int b){\n\treturn a + b;\n}\nint test(int c){\n\treturn c;\n}\nint main(){\n\tint i = foo(2, 3) + test(4);\n}\n"
+
+let testTypeInferencing _ =
+  (* Printf.printf "%s\n" @@ escape_chars @@ (inferencing_2 |> e2e); *)
+  assert_equal inferencing_1_c @@ (inferencing_1 |> e2e);
+  assert_equal inferencing_2_c @@ (inferencing_2 |> e2e)
+
+let test_issues _ =
+  (* Printf.printf "%s\n" @@ escape_chars @@ (e2e "def foo():\n\tprint(\'hello\')"); *)
+
+  (* Printf.printf "1\n"; *)
+  (* https://github.com/tjbai/p2c/issues/23 *)
+  assert_equal "int main(){\n\tint i = 0;\n\ti += 1;\n}\n" @@ e2e "i = 0\ni += 1" ;
+
+  (* https://github.com/tjbai/p2c/issues/21 *)
+  (* Printf.printf "2\n"; *)
+
+  assert_equal "void foo(){\n\tprintf(\"%s\", \"hello\");\n}\n"
+  @@ e2e "def foo():\n\tprint(\'hello\')";
+  (* Printf.printf "3\n"; *)
+
+  (* https://github.com/tjbai/p2c/issues/22 *)
+
+  (* Printf.printf "%s\n" @@ escape_chars @@ e2e "def foo():\n\ti = 0\n\ti += 1"; *)
+
+  assert_equal "void foo(){\n\tint i = 0;\n\ti += 1;\n}\n"
+  @@ e2e
+  "def foo():\n\ti = 0\n\ti += 1"
+  (* Printf.printf "4\n" *)
+  
+
+(* time permitting, the operator precedence feature would also be nice *)
+
+(**************************** TESTS **************************)
+let tests =
+  Printf.printf "%s\n" @@ escape_chars @@ "Placeholder"; (*placeholder so we don't have to comment and uncomment escape_chars for testing*)
+  "End to End Tests"
+  >: test_list
+       [
+         "testFunctions" >:: testFunctions;
+         "testIf" >:: testIf;
+         "testFuncCall" >:: testFuncCall;
+         "testFuncWithComments" >:: testFuncWithComments;
+         "testTypeInferencing" >:: testTypeInferencing;
+         "Issues" >:: test_issues;
+       ]
+
+let () = run_test_tt_main tests
